a147154199d3a4a6216486f6664f7409e93b4407
avery-joo <sohyeong.joo@kellogg.northwestern.edu>
2026-02-24 05:04:01 +0000

    places - index, show

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,19 @@
+class PlacesController < ApplicationController
+
+  def index
+    @places = Place.all
+  end
+
+  def show
+    @place = Place.find_by[{"id" => params["id"]}]
+    @entries = Entry.where({"place_id" => @place["id"]})
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+end
